c2413f902c4774ef99546a676d51bd93408ed852
shuiwenwuhen@gmail.com <970126.liu>
2020-11-27 16:15:52 +0800

    """ Test push """

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        题目：
+        构造一个函数，得到字符串中不重复的最大字串长度
+        input type s: str
+        output type : int
+        """
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+            print(st)
+        return ans
+
+
+if __name__ == "__main__":
+    test1 = 'abcabcdef'
+    test2 = "pwwkew"
+    test3 = " "
+    test4 = "bbbbbbb"
+    test5 = "dvdg"
+
+    sol = Solution()
+    length = sol.lengthOfLongestSubstring(test2)
+    print(length)
